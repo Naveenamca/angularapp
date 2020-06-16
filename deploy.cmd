6b76909a274c2c7c791db435a77278bf2cf2a821
Naveena <naveenamca.a@gmail.com>
2020-06-16 16:45:44 +0530

    added deployement file

--- deploy.cmd
+++ deploy.cmd
@@ -94,8 +94,8 @@ echo Handling node.js deployment.
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
